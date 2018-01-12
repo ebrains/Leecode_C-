6ca5efd00ee252b9885596ee82ae89d2d5aa5f2a
Haichen Zhu <hzhu3@stevens.edu>
2018-01-12 12:42:33 -0500

    022_Generate Parentheses

---
+++ 022_Generate Parentheses.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Program test = new Program();
+                IList<string> list = test.GenerateParenthesis(3);
+
+                foreach (string i in list)
+                {
+                    Console.Write(i + " ");
+                }
+
+                Console.ReadLine();
+            }
+
+            public IList<string> GenerateParenthesis(int n)
+            {
+                List<string> result = new List<string>();
+                if (n <= 0)
+                {
+                    return result;
+                }
+                helper(result, "", n, n);
+                return result;
+            }
+
+            //parameter for helper
+            // current parenteness
+            // how many left paren we need to add
+            // how many right paren we need to add
+            public void helper(IList<string> result, string parenteness, int left, int right)
+            {
+                if (left == 0 && right == 0)
+                {
+                    result.Add(parenteness);
+                    return;
+                }
+
+                if (left > 0)
+                {
+                    helper(result, parenteness + "(", left - 1, right);
+                }
+
+                if (right > 0 && left < right)
+                {
+                    helper(result, parenteness + ")", left, right - 1);
+                }
+            }
+
+            public IList<String> GenerateParenthesis2(int n)
+            {
+                List<String> ans = new List<String>();
+                if (n == 0)
+                {
+                    ans.Add("");
+                }
+                else
+                {
+                    for (int c = 0; c < n; ++c)
+                        foreach (String left in GenerateParenthesis2(c))
+                            foreach (String right in GenerateParenthesis2(n - 1 - c))
+                                ans.Add("(" + left + ")" + right);
+                }
+                return ans;
+            }
+        }
+}
