510b5c743ffb4f35d42555a76c9c235d07f59fee
Haichen Zhu <ebrains@outlook.com>
2018-02-15 15:55:32 -0500

    018 019
    018 019

---
+++ 018_4Sum.cs
@@ -0,0 +1,88 @@
+﻿/*
+Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
+Note: The solution set must not contain duplicate quadruplets.
+*/
+
+using System;
+using System.Collections.Generic;
+
+namespace Leetcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program test = new Program();
+            int[] s = { -1, 0, 1, 2, -1, -4,3 };
+
+            IList<IList<int>> list = test.FourSum(s,3);
+
+            foreach (IList<int> tmp in list)
+            {
+                Console.Write("( ");
+                foreach (int res in tmp)
+                {
+                    Console.Write(res + ", ");
+                }
+                Console.Write(")");
+                Console.WriteLine("");
+            }
+
+        }
+
+        public IList<IList<int>> FourSum(int[] nums, int target)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+            Array.Sort(nums);
+
+            for (int i = 0; i < nums.Length - 3; i++)
+            {
+                if (i != 0 && nums[i] == nums[i - 1])
+                {
+                    continue;
+                }
+                for (int j = i + 1; j < nums.Length - 2; j++)
+                {
+                    if (j != i + 1 && nums[j] == nums[j - 1])
+                        continue;
+                    int left = j + 1;
+                    int right = nums.Length - 1;
+                    while (left < right)
+                    {
+                        int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                        if (sum < target)
+                        {
+                            left++;
+                        }
+                        else if (sum > target)
+                        {
+                            right--;
+                        }
+                        else
+                        {
+                            List<int> tmp = new List<int>();
+                            tmp.Add(nums[i]);
+                            tmp.Add(nums[j]);
+                            tmp.Add(nums[left]);
+                            tmp.Add(nums[right]);
+                            result.Add(tmp);
+                            left++;
+                            right--;
+                            while (left < right && nums[left] == nums[left - 1])
+                            {
+                                left++;
+                            }
+                            while (left < right && nums[right] == nums[right + 1])
+                            {
+                                right--;
+                            }
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+    }   
+}
+
+
---
+++ 019_Remove Nth Node From End of List.cs
@@ -0,0 +1,108 @@
+﻿/*
+Given a linked list, remove the nth node from the end of list and return its head.
+For example,
+Given linked list: 1->2->3->4->5, and n = 2.
+After removing the second node from the end, the linked list becomes 1->2->3->5.
+*/
+
+using System;
+using System.Collections.Generic;
+
+namespace Leetcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program test = new Program();
+
+            ListNode list = new ListNode(2);
+            list.next = new ListNode(4);
+            list.next.next = new ListNode(5);
+            list.next.next.next = new ListNode(6);
+            printNode(list);
+
+            test.RemoveNthFromEnd(list, 2);
+            printNode(list);
+
+        }
+
+        //One pass algorithm
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            if (head == null)
+                return null;
+
+            ListNode fast = head;
+            ListNode slow = head;
+
+            for (int i = 0; i < n; i++)
+            {
+                fast = fast.next;
+            }
+
+            //if remove the first node
+            if (fast == null)
+            {
+                head = head.next;
+                return head;
+            }
+
+            while (fast.next != null)
+            {
+                fast = fast.next;
+                slow = slow.next;
+            }
+
+            slow.next = slow.next.next;
+
+            return head;
+        }
+
+        //Two pass algorithm
+        public ListNode RemoveNthFromEnd2(ListNode head, int n)
+        {
+            ListNode dummy = new ListNode(0);
+            dummy.next = head;
+            int length = 0;
+            ListNode first = head;
+            while (first != null)
+            {
+                length++;
+                first = first.next;
+            }
+            length -= n;
+            first = dummy;
+            while (length > 0)
+            {
+                length--;
+                first = first.next;
+            }
+            first.next = first.next.next;
+            return dummy.next;
+            
+        }
+
+        static void printNode(ListNode node)
+        {
+            while (node != null)
+            {
+                Console.Write( node.val + " -> ");
+                node = node.next;
+            }
+            Console.Write("NULL");
+            Console.WriteLine();
+        }
+
+    }  
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }   
+
+}
+
+
