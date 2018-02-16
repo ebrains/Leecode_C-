745e14579ceed6dfbe557953d7cba73b62574cb6
Haichen Zhu <ebrains@outlook.com>
2018-02-16 16:53:52 -0500

    021 024
    021 024

---
+++ 021_Merge Two Sorted Lists.cs
@@ -0,0 +1,99 @@
+﻿/*
+Merge two sorted linked lists and return it as a new list. 
+The new list should be made by splicing together the nodes of the first two lists.
+Example:
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+*/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Leetcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program test = new Program();
+            ListNode list1 = new ListNode(2);
+            list1.next = new ListNode(4);
+            list1.next.next = new ListNode(5);
+            list1.next.next.next = new ListNode(8);
+            ListNode list2 = new ListNode(3);
+            list2.next = new ListNode(6);
+            list2.next.next = new ListNode(7);
+            list2.next.next.next = new ListNode(9);
+            printNode(list1);
+            printNode(list2);
+
+            ListNode res =  test.MergeTwoLists(list1, list2);
+            printNode(res);
+
+        }
+
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            ListNode head = new ListNode(0);
+            ListNode l = head;
+
+            if(l1 == null)
+            {
+                return l2;
+            }
+            if(l2 == null)
+            {
+                return l1;
+            }
+
+            while(l1 != null && l2 != null)
+            {
+                if(l1.val <= l2.val)
+                {
+                    l.next = l1;
+                    l1 = l1.next;
+                }
+                else
+                {
+                    l.next = l2;
+                    l2 = l2.next;
+                }
+                l = l.next;
+            }
+
+            if(l1 != null)
+            {
+                l.next = l1;
+            }
+            if(l2 != null)
+            {
+                l.next = l2;
+            }
+
+            return head.next;
+                    
+        }
+
+        static void printNode(ListNode node)
+        {
+            while (node != null)
+            {
+                Console.Write(node.val + " -> ");
+                node = node.next;
+            }
+            Console.Write("NULL");
+            Console.WriteLine();
+        }
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+
+    }
+}
+
+
---
+++ 024_Swap Nodes in Pairs.cs
@@ -0,0 +1,72 @@
+﻿/*
+Given a linked list, swap every two adjacent nodes and return its head.
+For example,
+Given 1->2->3->4, you should return the list as 2->1->4->3.
+Your algorithm should use only constant space. You may not modify the values in the list, only nodes itself can be changed.
+*/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Leetcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program test = new Program();
+            ListNode list = new ListNode(2);
+            list.next = new ListNode(4);
+            list.next.next = new ListNode(5);
+            list.next.next.next = new ListNode(6);
+            printNode(list);
+
+            printNode(test.SwapPairs(list));
+
+        }
+
+        //  fake head -> 1 -> 2- >3   1 point to 3, 2 point to 1, fake point to 2
+        public ListNode SwapPairs(ListNode head)
+        {
+            if (head == null || head.next == null)
+                return head;
+
+            ListNode h = new ListNode(0);
+            h.next = head;
+            ListNode p = h;
+            while (p.next != null && p.next != null && p.next.next != null)
+            {
+                ListNode end = p.next.next;
+                p.next.next = end.next;
+                end.next = p.next;
+                p.next = end;
+
+                p = p.next.next;
+            }
+            return h.next;
+        }
+
+
+        static void printNode(ListNode node)
+        {
+            while (node != null)
+            {
+                Console.Write(node.val + " -> ");
+                node = node.next;
+            }
+            Console.Write("NULL");
+            Console.WriteLine();
+        }
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+
+    }
+}
+
+
