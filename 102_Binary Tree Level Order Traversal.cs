8c022996495fa2611d24225cf1ba7c5c843926af
Haichen Zhu <hzhu3@stevens.edu>
2018-03-14 16:33:46 -0400

    3.14

---
+++ 102_Binary Tree Level Order Traversal.cs
@@ -0,0 +1,66 @@
+﻿/*
+Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its bottom-up level order traversal as:
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+*/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Leetcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program test = new Program();
+        }
+
+        // Analysis: Use queue to track
+        public IList<IList<int>> LevelOrderBottom(TreeNode root)
+        {
+            var result = new List<IList<int>>();
+            if (root == null) return result;
+
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            while (queue.Count > 0)
+            {
+                var list = new List<int>();
+                var currentLevelNumber = queue.Count;
+                for (var i = 0; i < currentLevelNumber; i++)
+                {
+                    var node = queue.Dequeue();
+                    list.Add(node.val);
+                    if (node.left != null) queue.Enqueue(node.left);
+                    if (node.right != null) queue.Enqueue(node.right);
+                }
+                result.Insert(0, list);
+            }
+            return result;
+        }
+
+        //Definition for a binary tree node
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+        }
+    }
+}
+
+
---
+++ 107_Binary Tree Level Order Traversal II.cs
@@ -0,0 +1,68 @@
+﻿/*
+Given a binary tree, return the bottom-up level order traversal of its nodes' values. 
+(ie, from left to right, level by level from leaf to root).
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its bottom-up level order traversal as:
+[
+  [15,7],
+  [9,20],
+  [3]
+]
+*/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Leetcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program test = new Program();
+        }
+
+        // Analysis: Use queue to track  , reverse the list
+
+        public IList<IList<int>> LevelOrderBottom(TreeNode root)
+        {
+            var result = new List<IList<int>>();
+            if (root == null) return result;
+
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            while (queue.Count > 0)
+            {
+                var list = new List<int>();
+                var currentLevelNumber = queue.Count;
+                for (var i = 0; i < currentLevelNumber; i++)
+                {
+                    var node = queue.Dequeue();
+                    list.Add(node.val);
+                    if (node.left != null) queue.Enqueue(node.left);
+                    if (node.right != null) queue.Enqueue(node.right);
+                }
+                result.Insert(0, list);
+            }
+            return result;
+        }
+
+        //Definition for a binary tree node
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+        }
+    }
+}
+
+
