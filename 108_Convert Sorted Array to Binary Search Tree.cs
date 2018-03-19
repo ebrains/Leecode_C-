7a07d7d922b926aa0815b32a2f114b2b89c1e33b
Haichen Zhu <hzhu3@stevens.edu>
2018-03-19 09:43:45 -0400

    3.17

---
+++ 108_Convert Sorted Array to Binary Search Tree.cs
@@ -0,0 +1,66 @@
+﻿/*
+Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the
+two subtrees of every node never differ by more than 1.
+
+Example:
+
+Given the sorted array: [-10,-3,0,5,9],
+One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
+      0
+     / \
+   -3   9
+   /   /
+ -10  5
+*/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Leetcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program test = new Program();
+        }
+
+        // Solution: do it manually would be like this. find the mid and set it as root, 
+        //divide it into two parts, from the left part, find the mid and set it as root.left; from right part, find the mid and set it as root.right. It is recursion.
+        public TreeNode SortedArrayToBST(int[] nums)
+        {
+            return this.sortedArrayToBSTRecursion(nums, 0, nums.Length - 1);
+        }
+
+        private TreeNode sortedArrayToBSTRecursion(int[] num, int low, int high)
+        {
+            if (low > high)
+            {
+                return null;
+            }
+
+            var mid = (high - low) / 2 + low;
+
+            var newRoot = new TreeNode(num[mid])
+            {
+                left = this.sortedArrayToBSTRecursion(num, low, mid - 1),
+                right = this.sortedArrayToBSTRecursion(num, mid + 1, high)
+            };
+
+            return newRoot;
+        }
+
+        //Definition for a binary tree node
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+        }
+    }
+}
+
+
---
+++ 110_Balanced Binary Tree.cs
@@ -0,0 +1,89 @@
+﻿/*
+Given a binary tree, determine if it is height-balanced.
+For this problem, a height-balanced binary tree is defined as:
+a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+Example 1:
+Given the following tree [3,9,20,null,null,15,7]:
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+Return true.
+
+Example 2:
+Given the following tree [1,2,2,3,3,null,null,4,4]:
+
+       1
+      / \
+     2   2
+    / \
+   3   3
+  / \
+ 4   4
+Return false.
+*/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Leetcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program test = new Program();
+        }
+
+        /* Analysis: 
+         Solution1 :It is easy to make mistake here. compare the current node’s has left or has right to determine its binary tree. 
+         it should compare the height of left and height or right differ by no more than 1.
+
+         Solution2:  Combine get height and check is balance together within one traversal. use -1 to indicate it is invalid,
+         otherwise return the height
+        */
+        public bool IsBalanced(TreeNode root)
+        {
+            if (root == null)
+                return true;
+            if (getHeight(root) == -1)
+                return false;
+
+            return true;
+        }
+
+        public int getHeight(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+            int left = getHeight(root.left);
+            int right = getHeight(root.right);
+
+            if(left == -1 || right == -1)
+            {
+                return -1;
+            }
+            if (Math.Abs(left - right) > 1)
+            {
+                return -1;
+            }
+
+            return Math.Max(left, right) + 1;
+        }
+
+        //Definition for a binary tree node
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+        }
+    }
+}
+
+
