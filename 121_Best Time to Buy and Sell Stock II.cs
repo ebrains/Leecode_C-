cd39aea047bcfcc751e1c359ede6f3759a7e0784
Haichen Zhu <hzhu3@stevens.edu>
2018-04-11 11:35:31 -0400

    121,122
    121,122

---
+++ 121_Best Time to Buy and Sell Stock II.cs
@@ -0,0 +1,39 @@
+﻿/*
+Say you have an array for which the ith element is the price of a given stock on day i.
+Design an algorithm to find the maximum profit. 
+You may complete as many transactions as you like 
+(ie, buy one and sell one share of the stock multiple times).
+However, you may not engage in multiple transactions at the same time
+(ie, you must sell the stock before you buy again).
+
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Leetcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program test = new Program();
+        }
+
+        public int MaxProfit(int[] prices)
+        {
+            int profit = 0;
+            for (int i = 0; i<prices.Length - 1; i++)
+            {
+                int dif = prices[i + 1] - prices[i];
+                if(dif > 0)
+                {
+                    profit = profit + dif;
+                }
+            }
+            return profit;       
+        }
+    }
+}
+
---
+++ 122_Best Time to Buy and Sell Stock.cs
@@ -0,0 +1,45 @@
+﻿/*
+Say you have an array for which the ith element is the price of a given stock on day i.
+If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+Example 1:
+Input: [7, 1, 5, 3, 6, 4]
+Output: 5
+
+max. difference = 6-1 = 5 (not 7-1 = 6, as selling price needs to be larger than buying price)
+Example 2:
+Input: [7, 6, 4, 3, 1]
+Output: 0
+
+In this case, no transaction is done, i.e. max profit = 0.
+
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Leetcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program test = new Program();
+        }
+
+        public int MaxProfit(int[] prices)
+        {
+            int min = Int32.MaxValue;
+            int profit = 0;
+            
+            foreach(int i in prices)
+            {
+                min = i < min ? i : min;
+                profit = (i - min) > profit ? i - min : profit;
+            }
+            return profit;
+        }
+    }
+}
+
